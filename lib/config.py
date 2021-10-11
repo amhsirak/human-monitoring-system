864455306e9fcb2a21c35fb4cffb49b5b6a66f01
karishmas6 <carishmashukla28@gmail.com>
2021-10-11 21:01:35 +0530

    additional config

---
+++ lib/config.py
@@ -0,0 +1,25 @@
+# This file has all *optional* additional features' configuration
+
+# To recieve real-time email alerts. Eg: 'name@gmail.com'
+MAIL = ''
+
+# Enter IP camera url (Eg: url = 'http://191.138.0.100:8040/video'). Set to 0 for webcam.
+url = ''
+
+# ON/OFF for mail feature. Enter True to turn on the email alert feature.
+ALERT = False
+
+# Set max. people inside limit. Optimise number below: 10, 50, 100, etc.
+Threshold = 10
+
+# Threading ON/OFF
+Thread = False
+
+# Simple log to log the counting of people
+Log = False
+
+# Auto run/Schedule the software to run at your desired time
+Scheduler = False
+
+# Auto stop the software after certain a time/hours
+Timer = False
