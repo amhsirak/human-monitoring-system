8340f6efca82b44abc25f4da7a7555795b97e6de
karishmas6 <carishmashukla28@gmail.com>
2021-10-11 13:30:10 +0530

    cli arguments

---
+++ main.py
@@ -0,0 +1,18 @@
+from lib.centroidtracker import CentroidTracker
+from lib.trackableobject import TrackableObject
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse, imutils
+import time, schedule, csv
+import time, dlib, cv2, datetime
+from itertools import zip_longest
+import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-p', 'prototxt', required=True, help='Path to Caffe deploy prototxt file')
+ap.add_argument('-m', '--model', required=True, help='Path to Caffe pre-trained model')
+ap.add_argument('-i', '--input', type=str, help='Path to optional input video file')
+ap.add_argument('-o', '--ouput', type=str, help='Path to optional output video file')
+ap.add_argument('-c', '--confidence', type=float, default=0.4, help='Minimum probability to filter weak detections')
+ap.add_argument('-s', '--skip-frames', type=int, default=30, help='# of skip frames between detections')
+args = vars(ap.parse_args())
