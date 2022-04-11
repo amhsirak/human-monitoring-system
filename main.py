a58694215ba8463be2988814ebe4f09f55452dce
karishmas6 <carishmashukla28@gmail.com>
2022-04-12 05:10:40 +0530

    final code

--- main.py
+++ main.py
@@ -16,48 +16,41 @@ def run():
 
 	# construct the argument parse and parse the arguments
 	ap = argparse.ArgumentParser()
-	ap.add_argument('-p', '--prototxt', required=False,
-		help='path to Caffe deploy prototxt file')
-	ap.add_argument('-m', '--model', required=True,
-		help='path to Caffe pre-trained model')
-	ap.add_argument('-i', '--input', type=str,
-		help='path to optional input video file')
-	ap.add_argument('-o', '--output', type=str,
-		help='path to optional output video file')
-	ap.add_argument('-c', '--confidence', type=float, default=0.4,
-		help='minimum probability to filter weak detections')
-	ap.add_argument('-s', '--skip-frames', type=int, default=30,
-		help='# of skip frames between detections')
-	
-	# Other features
-	ap.add_argument('-l', '--log', type=bool, default=False, help='Simple log to log the counting of people')
-	ap.add_argument('-a', '--alert', type=bool, default=False, help='Enter True to turn on the email alert feature')
-	ap.add_argument('-thresh', '--threshold', type=int, default=3, help='Set the limit for maximum people inside')
-	ap.add_argument('-th', '--threading', type=bool, default=False, help='Turn threading on/off')
-	ap.add_argument('-t', '--timer', type=bool, default=False, help='Auto stop the software after certain time')
-	ap.add_argument('-tym', '--time', type=float, default=28800, help='Seconds after which the software must be auto stopped')
+	ap.add_argument("-p", "--prototxt", required=False,
+		help="path to Caffe 'deploy' prototxt file")
+	ap.add_argument("-m", "--model", required=True,
+		help="path to Caffe pre-trained model")
+	ap.add_argument("-i", "--input", type=str,
+		help="path to optional input video file")
+	ap.add_argument("-o", "--output", type=str,
+		help="path to optional output video file")
+	# confidence default 0.4
+	ap.add_argument("-c", "--confidence", type=float, default=0.4,
+		help="minimum probability to filter weak detections")
+	ap.add_argument("-s", "--skip-frames", type=int, default=30,
+		help="# of skip frames between detections")
 	args = vars(ap.parse_args())
 
 	# initialize the list of class labels MobileNet SSD was trained to
 	# detect
-	CLASSES = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
-		'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
-		'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep',
-		'sofa', 'train', 'tvmonitor']
+	CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+		"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+		"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+		"sofa", "train", "tvmonitor"]
 
 	# load our serialized model from disk
-	net = cv2.dnn.readNetFromCaffe(args['prototxt'], args['model'])
+	net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 	# if a video path was not supplied, grab a reference to the ip camera
-	if not args.get('input', False):
-		print('[INFO] Starting the live stream...')
+	if not args.get("input", False):
+		print("[INFO] Starting the live stream..")
 		vs = VideoStream(config.url).start()
 		time.sleep(2.0)
 
 	# otherwise, grab a reference to the video file
 	else:
-		print('[INFO] Starting the video...')
-		vs = cv2.VideoCapture(args['input'])
+		print("[INFO] Starting the video..")
+		vs = cv2.VideoCapture(args["input"])
 
 	# initialize the video writer (we'll instantiate later if need be)
 	writer = None
@@ -80,13 +73,13 @@ def run():
 	totalDown = 0
 	totalUp = 0
 	x = []
-	emptyExit=[]
-	emptyEntry=[]
+	empty=[]
+	empty1=[]
 
 	# start the frames per second throughput estimator
 	fps = FPS().start()
 
-	if args['threading'] == True:
+	if config.Thread:
 		vs = thread.ThreadingClass(config.url)
 
 	# loop over frames from the video stream
@@ -94,11 +87,11 @@ def run():
 		# grab the next frame and handle if we are reading from either
 		# VideoCapture or VideoStream
 		frame = vs.read()
-		frame = frame[1] if args.get('input', False) else frame
+		frame = frame[1] if args.get("input", False) else frame
 
 		# if we are viewing a video and we did not grab a frame then we
 		# have reached the end of the video
-		if args['input'] is not None and frame is None:
+		if args["input"] is not None and frame is None:
 			break
 
 		# resize the frame to have a maximum width of 500 pixels (the
@@ -113,22 +106,22 @@ def run():
 
 		# if we are supposed to be writing a video to disk, initialize
 		# the writer
-		if args['output'] is not None and writer is None:
-			fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-			writer = cv2.VideoWriter(args['output'], fourcc, 30,
+		if args["output"] is not None and writer is None:
+			fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+			writer = cv2.VideoWriter(args["output"], fourcc, 30,
 				(W, H), True)
 
 		# initialize the current status along with our list of bounding
 		# box rectangles returned by either (1) our object detector or
 		# (2) the correlation trackers
-		status = 'Waiting'
+		status = "Waiting"
 		rects = []
 
 		# check to see if we should run a more computationally expensive
 		# object detection method to aid our tracker
-		if totalFrames % args['skip_frames'] == 0:
+		if totalFrames % args["skip_frames"] == 0:
 			# set the status and initialize our new set of object trackers
-			status = 'Detecting'
+			status = "Detecting"
 			trackers = []
 
 			# convert the frame to a blob and pass the blob through the
@@ -145,19 +138,19 @@ def run():
 
 				# filter out weak detections by requiring a minimum
 				# confidence
-				if confidence > args['confidence']:
+				if confidence > args["confidence"]:
 					# extract the index of the class label from the
 					# detections list
 					idx = int(detections[0, 0, i, 1])
 
 					# if the class label is not a person, ignore it
-					if CLASSES[idx] != 'person':
+					if CLASSES[idx] != "person":
 						continue
 
 					# compute the (x, y)-coordinates of the bounding box
 					# for the object
 					box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-					(startX, startY, endX, endY) = box.astype('int')
+					(startX, startY, endX, endY) = box.astype("int")
 
 
 					# construct a dlib rectangle object from the bounding
@@ -178,7 +171,7 @@ def run():
 			for tracker in trackers:
 				# set the status of our system to be 'tracking' rather
 				# than 'waiting' or 'detecting'
-				status = 'Tracking'
+				status = "Tracking"
 
 				# update the tracker and grab the updated position
 				tracker.update(rgb)
@@ -196,9 +189,9 @@ def run():
 		# draw a horizontal line in the center of the frame -- once an
 		# object crosses this line we will determine whether they were
 		# moving 'up' or 'down'
-		cv2.line(frame, (0, H // 2), (W, H // 2), (255,255,255), 2)
-		cv2.putText(frame, 'Prediction Border(Entrance)', (10, H - ((i * 20) + 200)),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
+		cv2.line(frame, (0, H // 2), (W, H // 2), (0, 0, 0), 3)
+		cv2.putText(frame, "-Prediction border - Entrance-", (10, H - ((i * 20) + 200)),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 
 		# use the centroid tracker to associate the (1) old object
 		# centroids with (2) the newly computed object centroids
@@ -232,7 +225,7 @@ def run():
 					# line, count the object
 					if direction < 0 and centroid[1] < H // 2:
 						totalUp += 1
-						emptyExit.append(totalUp)
+						empty.append(totalUp)
 						to.counted = True
 
 					# if the direction is positive (indicating the object
@@ -240,62 +233,64 @@ def run():
 					# center line, count the object
 					elif direction > 0 and centroid[1] > H // 2:
 						totalDown += 1
-						emptyEntry.append(totalDown)
-						
+						empty1.append(totalDown)
+						#print(empty1[-1])
 						# if the people limit exceeds over threshold, send an email alert
-						if sum(x) >= args['threshold']:
-							cv2.putText(frame, 'ALERT-People limit exceeded!', (10, frame.shape[0] - 80),
-								cv2.FONT_HERSHEY_COMPLEX, 0.5, (3, 252, 32), 2)
-							if args['alert'] == True:
-								print('[INFO] Sending email alert...')
+						if sum(x) >= config.Threshold:
+							cv2.putText(frame, "-ALERT: People limit exceeded-", (10, frame.shape[0] - 80),
+								cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
+							if config.ALERT:
+								print("[INFO] Sending email alert..")
 								Mailer().send(config.MAIL)
-								print('[INFO] Alert sent.')
+								print("[INFO] Alert sent")
 
 						to.counted = True
 						
 					x = []
 					# compute the sum of total people inside
-					x.append(len(emptyEntry)-len(emptyExit))
+					x.append(len(empty1)-len(empty))
+					#print("Total people inside:", x)
+
 
 			# store the trackable object in our dictionary
 			trackableObjects[objectID] = to
 
 			# draw both the ID of the object and the centroid of the
 			# object on the output frame
-			text = 'ID {}'.format(objectID)
+			text = "ID {}".format(objectID)
 			cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (3, 252, 32), 2)
-			cv2.circle(frame, (centroid[0], centroid[1]), 4, (3, 252, 32), -1)
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+			cv2.circle(frame, (centroid[0], centroid[1]), 4, (255, 255, 255), -1)
 
 		# construct a tuple of information we will be displaying on the
 		info = [
-		('Exit', totalUp),
-		('Enter', totalDown),
-		('Status', status),
+		("Exit", totalUp),
+		("Enter", totalDown),
+		("Status", status),
 		]
 
 		info2 = [
-		('Total People Inside',x),
+		("Total people inside", x),
 		]
 
-        # Display the output
+                # Display the output
 		for (i, (k, v)) in enumerate(info):
-			text = '{}: {}'.format(k, v)
-			cv2.putText(frame, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 2)
+			text = "{}: {}".format(k, v)
+			cv2.putText(frame, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
 
 		for (i, (k, v)) in enumerate(info2):
-			text = '{}: {}'.format(k, v)
-			cv2.putText(frame, text, (255, H - ((i * 20) + 60)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 2)
+			text = "{}: {}".format(k, v)
+			cv2.putText(frame, text, (265, H - ((i * 20) + 60)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
 		# Initiate a simple log to save data at end of the day
-		if args['log'] == True:
+		if config.Log:
 			datetimee = [datetime.datetime.now()]
-			d = [datetimee, emptyEntry, emptyExit, x]
+			d = [datetimee, empty1, empty, x]
 			export_data = zip_longest(*d, fillvalue = '')
 
 			with open('Log.csv', 'w', newline='') as myfile:
 				wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-				wr.writerow(('End Time', 'In', 'Out', 'Total People Inside'))
+				wr.writerow(("End Time", "In", "Out", "Total Inside"))
 				wr.writerows(export_data)
 				
 		# check to see if we should write the frame to disk
@@ -303,9 +298,11 @@ def run():
 			writer.write(frame)
 
 		# show the output frame
-		cv2.imshow('Real-Time Human Monitoring', frame)
+		cv2.imshow("Real-Time Monitoring/Analysis Window", frame)
 		key = cv2.waitKey(1) & 0xFF
-		if key == ord('q'):
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
 			break
 
 		# increment the total number of frames processed thus far and
@@ -313,27 +310,30 @@ def run():
 		totalFrames += 1
 		fps.update()
 
-		if args['timer'] == True:
+		if config.Timer:
 			# Automatic timer to stop the live stream. Set to 8 hours (28800s).
 			t1 = time.time()
 			num_seconds=(t1-t0)
-			if num_seconds > args['time']:
+			if num_seconds > 28800:
 				break
 
 	# stop the timer and display FPS information
 	fps.stop()
-	print('[INFO] elapsed time: {:.2f}'.format(fps.elapsed()))
-	print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
+	print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-	if args['threading'] == True:
+	if config.Thread:
 		vs.release()
 
+	# close any open windows
 	cv2.destroyAllWindows()
 
 
+# https://pypi.org/project/schedule/
 if config.Scheduler:
-	# Runs everyday at 9:00AM. Can be changed.
-	schedule.every().day.at(config.SCHEDULING_TIME).do(run)
+	# Runs every day (09:00 am). You can change it.
+	schedule.every().day.at("09:00").do(run)
+
 	while 1:
 		schedule.run_pending()
 else:
